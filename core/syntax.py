6c39037a9f1751dc698470f283631829dae91e9a
python3lover <35400564+python3lover@users.noreply.github.com>
2018-02-01 11:01:46 -0500

    syntax.py (Work In Progress)
    + syntax.__init__
    + syntax.decode (WIP)

--- core/syntax.py
+++ core/syntax.py
@@ -1,1 +1,13 @@
 class syntax():
+  def __init__(self,path):
+    self.path = path
+    rfiletmp = open(path,'r')
+    self.source = rfiletmp.read()
+    rfiletmp.close()
+    del rfiletmp
+    self.lines = self.source.split('\n')
+    self.wfile = open(self.path,'w')
+  def decode(self):
+    for line in range(len(self.lines)):
+      source = self.lines[line]
+      ## Work In Progress
